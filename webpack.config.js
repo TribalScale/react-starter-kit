27fee93fd6358fd68574072018f70a00d303d504
Tim Liu <tliu@tribalscale.com>
2016-10-07 14:19:39 -0400

    [AM, TL] Added production build configurations and other minor cleanups
    Signed-off-by: Anoop Madhavan <amadhavan@tribalscale.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
-var path = require('path');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const DEBUG = (process.argv.indexOf('--release') === -1);
 
 module.exports = {
   entry: {
     app: ['babel-polyfill', 'isomorphic-fetch', './src/main.jsx']
   },
   output: {
-    path: path.resolve(__dirname),
-    filename: 'js/bundle.js'
+    path: path.resolve(__dirname, "build"),
+    filename: DEBUG ? 'js/bundle.js' : 'js/bundle.[hash].js'
   },
   resolve: {extensions: ['', '.js', '.jsx']},
   module: {
@@ -23,10 +27,28 @@ module.exports = {
       {
         test: /\.(scss|css)$/,
         loader: ExtractTextPlugin.extract('css!sass')
+      },
+      {
+        test: /\.(eot|svg|png|ttf|woff|woff2)$/,
+        loader: "url-loader?limit=100000&name=images/[name].[ext]"
       }
     ]
   },
   plugins: [
-    new ExtractTextPlugin('css/style.css')
+    new ExtractTextPlugin(DEBUG ? 'css/style.css' : 'css/style.[hash].css'),
+    new webpack.DefinePlugin({
+      "process.env": {
+         NODE_ENV: JSON.stringify(DEBUG ? "development" : "production")
+       }
+    }),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      hash: DEBUG ? false : true,
+      filename: 'index.html',
+      inject: 'body',
+      minify: {
+        collapseWhitespace: DEBUG ? false : true
+      }
+    })
   ]
 };
