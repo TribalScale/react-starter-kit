6544e570dd0473fcc24ab05c6e9435224707f94e
Tim Liu <tliu@tribalscale.com>
2016-10-20 15:51:10 -0400

    [#132788187][TL,AM] optimize the response handling
    Signed-off-by: Anoop Madhavan <amadhavan@tribalscale.com>

--- src/utils/async_action.js
+++ src/utils/async_action.js
@@ -1,15 +1,39 @@
+function responseParser(response, dispatch, params) {
+  const contentType = response.headers.get('content-type');
+  let fetchReponse;
+
+  if (!contentType) {
+    fetchReponse = response.blob();
+  } else if (contentType.indexOf('application/json') !== -1) {
+    fetchReponse = response.json();
+  } else if (contentType.indexOf('text/plain') !== -1) {
+    fetchReponse = response.text();
+  } else if (contentType.indexOf('multipart/form-data') !== -1) {
+    fetchReponse = response.formData();
+  } else {
+    fetchReponse = response.blob();
+  }
+  return fetchReponse.then(result => dispatch(params.receive_action(params.context, result)));
+}
+
 function makeAsyncCall(params) {
   return (dispatch) => {
-    let options = {};
-    dispatch(params.request_action(params.data));
+    let fetchInit = {
+      method: 'GET'
+    };
+    dispatch(params.request_action(params.context));
 
-    if (params.headers) {
-      options = Object.assign({}, { headers: params.headers });
+    if (params.fetchInit) {
+      fetchInit = params.fetchInit;
     }
 
-    return fetch(params.endpoint, options)
-    .then(response => response.json())
-    .then(json => dispatch(params.receive_action(params.data, json)));
+    return fetch(params.endpoint, fetchInit)
+    .then((response) => {
+      if (params.fetchResponseParser) {
+        return params.fetchResponseParser(response, dispatch, params);
+      }
+      return responseParser(response, dispatch, params);
+    });
   };
 }
 
