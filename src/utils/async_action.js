7658354cb356b08229276c2a9a5dedfa76919745
Tim Liu <tliu@tribalscale.com>
2016-10-20 17:16:01 -0400

    [#132787169][TL,AM] integrate error handling
    Signed-off-by: Anoop Madhavan <amadhavan@tribalscale.com>

--- src/utils/async_action.js
+++ src/utils/async_action.js
@@ -1,4 +1,9 @@
 function responseParser(response, dispatch, params) {
+  // INFO: Pass fetchResponseParser function in params for custom handling of response
+  if (params.fetchResponseParser) {
+    return params.fetchResponseParser(response, dispatch, params);
+  }
+
   const contentType = response.headers.get('content-type');
   let fetchReponse;
 
@@ -16,6 +21,17 @@ function responseParser(response, dispatch, params) {
   return fetchReponse.then(result => dispatch(params.receive_action(params.context, result)));
 }
 
+function errorHandling(response, params) {
+  if (!response.ok) {
+    // INFO: Pass err function in params for custom handling of errors
+    if (params.err) {
+      params.err(response);
+    }
+    throw Error(response.statusText);
+  }
+  return response;
+}
+
 function makeAsyncCall(params) {
   return (dispatch) => {
     let fetchInit = {
@@ -28,11 +44,12 @@ function makeAsyncCall(params) {
     }
 
     return fetch(params.endpoint, fetchInit)
-    .then((response) => {
-      if (params.fetchResponseParser) {
-        return params.fetchResponseParser(response, dispatch, params);
+    .then(response => errorHandling(response, params))
+    .then(response => responseParser(response, dispatch, params))
+    .catch((error) => {
+      if (!params.err) {
+        console.log(error);
       }
-      return responseParser(response, dispatch, params);
     });
   };
 }
