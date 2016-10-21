deb9ece0cdd5511c13fcf39ace9852a7d7009395
Tim Liu <tliu@tribalscale.com>
2016-10-21 12:05:21 -0400

    [#132875443][TL,AM] mocked async actions in actions unit tests
    Signed-off-by: Anoop Madhavan <amadhavan@tribalscale.com>

--- tests/actions/sample_user_test.js
+++ tests/actions/sample_user_test.js
@@ -1,18 +1,48 @@
-import { testAsyncAction } from '../test_utils';
+import sinon from 'sinon';
+import fetchMock from 'fetch-mock';
+import expect from 'unexpected';
 
-import { REQUEST_USER, RECEIVE_USER, fetchSampleUser } from '../../src/actions/sample_user';
+import * as asyncAction from '../../src/utils/async_action';
+import { fetchSampleUser } from '../../src/actions/sample_user';
 
+describe("Sample User Actions", () => {
+  const context = {
+    query: 'Random query'
+  };
+  let argObj;
 
-// INFO: Sample to test async action.
-// You can use the following approach if you are using async_action util in the app
-const params = {
-  testDescribe: 'Sample User Actions',
-  actionMethod: fetchSampleUser,
-  uri: 'https://api.randomuser.me',
-  endpoint: '/?results=15',
-  method: 'GET',
-  returnsOnRequest: { type: REQUEST_USER, context: undefined },
-  returnsOnReceive: { type: RECEIVE_USER, response: { hello: 'world' } }
-};
+  before(() => {
+    const spy = sinon.spy(asyncAction, "default");
 
-testAsyncAction(params);
+    fetchMock.get('*', {response: 'Sample User details'});
+    fetchSampleUser(context);
+    fetchMock.restore();
+
+    argObj = spy.args[0][0];
+
+  });
+
+  it("should get an object argument with specific keys", () => {
+    expect(argObj, 'to have keys', ['request_action', 'receive_action', 'endpoint', 'reducer', 'context']);
+  });
+
+  it("should have a request action property that is a function", () => {
+    expect(argObj.request_action, 'to be a', 'function');
+  });
+
+  it("should have a receive action property that is a function", () => {
+    expect(argObj.receive_action, 'to be a', 'function');
+  });
+
+  it("should have a specific endpoint", () => {
+    expect(argObj.endpoint, 'to be', 'https://api.randomuser.me/?results=15');
+  });
+
+  it("should be associated with the user reducer", () => {
+    expect(argObj.reducer, 'to be', 'user');
+  });
+
+  it("should have context property matching the argument passed to fetchSampleUser", function() {
+    expect(argObj.context, 'to be', context);
+  });
+});
